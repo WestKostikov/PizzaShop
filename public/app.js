3615b04feb7e6970d099bec3c8301a3a881eb2f3
WestKostikov <maksrey@mail.ru>
2020-09-18 18:58:42 +0300

    Keep x variable in localStorage

--- public/app.js
+++ public/app.js
@@ -1,9 +1,12 @@
 function something()
 {
 
-	var x = window.localStorage.getItem('score');
+	var x = window.localStorage.getItem('score'); // это как x = hh['score'] в ruby
 
-	window.localStorage.setItem('score', 555);
+	// x * 1 - чтобы преобразовать строку в число
+	x = x * 1 + 1;
+
+	window.localStorage.setItem('score', x); // hh['score'] = x
 
   	alert(x);
   
